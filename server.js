62f4ab70d5316cb8b4a6e03b2f8acb8353c22ceb
Vedhashree-B-Babu <vedanagu100@gmail.com>
2017-03-04 00:19:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("hi this is article one")
+  res.send("hi this is article one");
 });
 
 app.get('/ui/madi.png', function (req, res) {
