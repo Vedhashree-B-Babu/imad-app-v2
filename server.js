c3472a9daf41fe95f1c6744fbccacf40bf1d4cc5
Vedhashree-B-Babu <vedanagu100@gmail.com>
2017-03-04 14:28:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('ui/article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','article-one.html'));
 });
-app.get('ui/article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','article-two.html'));
 });
 
- app.get('ui/article-one', function (req, res) {
+ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','article-three.html'));
  });
  
