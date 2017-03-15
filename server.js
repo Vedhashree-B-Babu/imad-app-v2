fb24225669c68db1278467214c82d1134fe3b850
Vedhashree-B-Babu <vedanagu100@gmail.com>
2017-03-15 18:13:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 var config = {
     user: 'vedhashree-b-babu',
     database: 'vedhashree-b-babu',
@@ -72,7 +72,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new pg.pool(config);
+var pool = new Pool(config);
 
 
 app.get('/test-db', function (req, res) {
