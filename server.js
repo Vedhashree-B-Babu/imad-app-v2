c60fcdc37744037e74594ee716ccf54df2784f34
Vedhashree-B-Babu <vedanagu100@gmail.com>
2017-03-18 22:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 var pool = new Pool(config);
 
 
